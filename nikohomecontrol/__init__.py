e1ed375965ada0ad9d04a6102443a72834214a0e
Levi Govaerts <lgovaerts@reference.be>
2018-10-30 20:36:28 +0100

    Fixed an issue where a lot of actions were blocking

--- nikohomecontrol/__init__.py
+++ nikohomecontrol/__init__.py
@@ -9,6 +9,7 @@
 
 import socket
 import json
+import time
 from io import StringIO
 
 class NikoHomeControl:
@@ -37,9 +38,32 @@ class NikoHomeControl:
     def execute_actions(self, id, value):
         return self._command('{"cmd":"executeactions", "id": "'+id+'", "value": "'+value+'"}')
 
+    def recv_timeout(self, timeout=2):
+        self._socket.setblocking(0)
+        total_data = [];
+        data = '';
+        begin = time.time()
+        while 1:
+            # if you got some data, then break after wait sec
+            if total_data and time.time() - begin > timeout:
+                break
+            # if you got no data at all, wait a little longer
+            elif time.time() - begin > timeout * 2:
+                break
+            try:
+                data = self._socket.recv(8192)
+                if data:
+                    total_data.append(data)
+                    begin = time.time()
+                else:
+                    time.sleep(0.1)
+            except:
+                pass
+        return ''.join(total_data)
+
     def _command(self, cmd):
         self._socket.send(cmd.encode())
-        data = json.load(StringIO(self._socket.recv(4096).decode()))
+        data = json.loads(self.recv_timeout())
 
         if ('error' in data['data'] and data['data']['error'] > 0):
             error = json['data']['error']
