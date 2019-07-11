64d87737c78af6d3df24f9dd82e6159ec9e12046
LGOVAERT <levi.govaerts@vdab.be>
2019-07-11 23:42:56 +0200

    Introduced thermostat functionalities

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setuptools.setup(
     description='SDK for Niko Home Control',
     license='MIT',
     url='https://github.com/NoUseFreak/niko-home-control',
-    version='0.2.1',
+    version='0.2.2',
     author='Dries De Peuter',
     author_email='dries@nousefreak.be',
     maintainer='Dries De Peuter',
